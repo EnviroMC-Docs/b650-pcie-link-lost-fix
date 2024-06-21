75fb4c56113b95c16cae0c20ef1a4be803d20e17
WoodsyTheNerd <119528792+WoodsyTheNerd@users.noreply.github.com>
2024-06-21 14:26:51 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
-# Download the script
-curl -o setup.sh https://github.com/EnviroMC-Docs/b650-pcie-link-lost-fix/raw/main/setup.sh
+#!/bin/bash
 
-# Make the script executable
-chmod +x setup.sh
+# URL of the setup.sh script
+SETUP_URL="https://github.com/EnviroMC-Docs/b650-pcie-link-lost-fix/raw/main/setup.sh"
 
-# Run the script as superuser
-sudo ./setup.sh
+# Download setup.sh
+curl -sL $SETUP_URL -o /tmp/setup.sh
+
+# Ensure the script is executable
+chmod +x /tmp/setup.sh
+
+# Run the script with root permissions
+sudo /tmp/setup.sh
